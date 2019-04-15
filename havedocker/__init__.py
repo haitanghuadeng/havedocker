b877bd7bad71f53ede897fc51002b2cda1b428ee
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2019-04-15 16:22:06 +0800

    Create __init__.py

---
+++ havedocker/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+
+from .have_docker import Agility_Docker
+
+name = "have_docker"
+version = '0.0.3'
+author = 'haitanghuadeng'
+description = "Repair havedocker exists in the BUG [Agility_Docker]"
+
