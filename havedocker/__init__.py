3bb3fe20333b2ffae074ca44c3159f5ccdcf3e40
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2019-04-15 20:41:15 +0800

    #0.1.3.2更新声明

--- havedocker/__init__.py
+++ havedocker/__init__.py
@@ -4,7 +4,7 @@
 from .have_docker import Agility_Docker
 
 name = "have_docker"
-version = '0.0.3'
+version = '0.1.3.2'
 author = 'haitanghuadeng'
 description = "Repair havedocker exists in the BUG [Agility_Docker]"
 
