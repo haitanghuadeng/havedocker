67004487ea888cd417837b3cdedaa579d3954d6a
With the light, as low as dust <43346455+haitanghuadeng@users.noreply.github.com>
2019-04-19 18:38:04 +0800

    New havedocker!

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="havedocker",
-    version="0.1.3.1",
+    version="0.1.3.5",
     author="haitanghuadeng",
     author_email="491609917@qq.com",
-    description="Repair havedocker exists in the BUG [Agility_Docker]",
+    description="New havedocker, already has the basic docker instruction",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/haitanghuadeng",
@@ -20,5 +20,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
